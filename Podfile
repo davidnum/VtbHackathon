e68769739dee9191b878b8162d89869fce2043c7
davidnum <davidnum95@gmail.com>
2020-10-09 23:32:21 +0300

    add ppods, first screen

---
+++ Podfile
@@ -0,0 +1,19 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'VtbHackathon' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for VtbHackathon
+
+  target 'VtbHackathonTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'VtbHackathonUITests' do
+    # Pods for testing
+  end
+
+end
--- VtbHackathon/ContentView.swift
+++ VtbHackathon/ContentView.swift
@@ -8,11 +8,65 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    enum Screen {
+        case Favorites, Main, Profile
+    }
+    
+    let bounds = UIScreen.main.bounds
+    @State var currentScreen = Screen.Main
+    
+    init() {
+        let appearance = UINavigationBarAppearance()
+        appearance.shadowColor = .clear
+        appearance.backgroundColor = .white
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            TabView(selection: $currentScreen) {
+                // Favorites
+                VStack {
+                    Text("Favorites")
+                }.tag(Screen.Favorites)
+                
+                // Main
+                VStack {
+                    Text("Нажмите, чтобы определить машину")
+                        .fontWeight(.semibold)
+                        .font(.system(size: 22.0))
+                        .padding(.horizontal, 80)
+                        .padding(.bottom, 48)
+                        .multilineTextAlignment(.center)
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                        Image("CameraButton")
+                    }
+                    .animation(.easeInOut(duration: 0.4))
+                    .buttonStyle(ScaleButtonStyle())
+                }.tag(Screen.Main)
+                
+                // Profile
+                VStack {
+                    Text("Profile")
+                }
+                .tag(Screen.Profile)
+            }
+            .tabViewStyle(PageTabViewStyle())
+            .navigationBarTitle("Хлавная", displayMode: .inline)
+        }
     }
 }
+
+struct ScaleButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .scaleEffect(configuration.isPressed ? 0.9 : 1)
+            .opacity(configuration.isPressed ? 0.3 : 1)
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
