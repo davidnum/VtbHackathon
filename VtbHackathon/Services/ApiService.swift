3d2e2a9a457040b9dd011e9ec186b701c9d5ebf4
Шалаумов Эмиль Владимирович <EVlShalaumov@gmail.com>
2020-10-10 22:59:06 +0300

    Добавил метод распознавания авто

--- VtbHackathon/Services/ApiService.swift
+++ VtbHackathon/Services/ApiService.swift
@@ -16,17 +16,29 @@ protocol ApiServiceProtocol {
 
 final class ApiService: ApiServiceProtocol {
 
-	func getMarketplaceList(completion: @escaping (Result<[CarBrandDataModel], Error>) -> Void) {
-		let headers: HTTPHeaders = [
-			"x-ibm-client-id": "e0a9ee9438e7c78ec75742a2c331422f",
-			"Accept": "application/json"
-		]
+	let headers: HTTPHeaders = [
+		"x-ibm-client-id": "e0a9ee9438e7c78ec75742a2c331422f",
+		"Accept": "application/json"
+	]
+
+	func getMarketplaceList(completion: @escaping (Result<[CarBrandDataModel], AFError>) -> Void) {
 		AF.request("https://gw.hackathon.vtb.ru/vtb/hackathon/marketplace",
 				   method: .get,
 				   headers: headers)
 			.responseDecodable(of: [String: [CarBrandDataModel]].self) { response in
-				guard let cars = response.value else { return }
+				guard let cars = response.value else {
+					return
+				}
 				completion(.success(cars["list"] ?? []))
 		}
+	}
+
+	func recognizeCar(by image: String, completion: @escaping (Result<[String: Double], Error>) -> Void) {
+		AF.request("https://gw.hackathon.vtb.ru/vtb/hackathon/car-recogniz",
+				   method: .post,
+				   headers: headers).responseDecodable(of: [String: [String: Double]].self) { response in
+					guard let probabilities = response.value else { return }
+					completion(.success(probabilities["probabilities"] ?? [:]))
+				}
 	}
 }
